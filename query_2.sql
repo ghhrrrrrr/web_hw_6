1ccc0c5a7db353555a7f4bffe63b9cd1bb9b976a
ghhrrrrrr <145476102+ghhrrrrrr@users.noreply.github.com>
2025-07-11 15:40:52 +0200

    Update query_2.sql

--- query_2.sql
+++ query_2.sql
@@ -1,8 +1,8 @@
- SELECT sname, max(avg_grade) 
-    FROM
-    (SELECT students.name AS sname, 
-    avg(grades.grade) AS avg_grade
-    FROM grades 
-    INNER JOIN students ON grades.student_id = students.id
-    WHERE subject_id = 1
-    GROUP BY students.id);
+SELECT students.name AS sname,
+       avg(grades.grade) AS avg_grade
+FROM grades
+JOIN students ON grades.student_id = students.id
+WHERE subject_id = 1
+GROUP BY students.id
+ORDER BY avg_grade DESC
+LIMIT 1;
